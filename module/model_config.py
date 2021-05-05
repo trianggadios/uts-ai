eaa4060d873a64b58b2446eccede7b89cc908b4a
trianggadios <trianggadio@gmail.com>
2021-05-06 00:10:42 +0700

    redownload if timeout

--- module/model_config.py
+++ module/model_config.py
@@ -9,15 +9,32 @@ def download_all_model():
     except:
         pass
 
-    GoogleDriveDownloader.download_file_from_google_drive(
-        file_id='1_CT6eSu63pfFdq4dXeFlV6OYjUBKfHAn',
-        dest_path='model/model.d2v',
-        showsize=True
+    model_1 = '1_CT6eSu63pfFdq4dXeFlV6OYjUBKfHAn'
+    model_1_des = 'model/model.d2v'
 
-    )
+    model_2 = '1-0pByu6ljf1JwrGiQSvXnMBOBrnxh-VU'
+    model_2_des = 'model/model_final.sav'
 
+    try:
+        GoogleDriveDownloader.download_file_from_google_drive(
+            file_id=model_1,
+            dest_path=model_1_des
+        )
+    except:
+        redownload(model_1, model_1_des)
+
+    try:
+        GoogleDriveDownloader.download_file_from_google_drive(
+            file_id=model_2,
+            dest_path=model_2_des
+        )
+    except:
+        redownload(model_2, model_2_des)
+
+
+def redownload(file_id, destination):
     GoogleDriveDownloader.download_file_from_google_drive(
-        file_id='1-0pByu6ljf1JwrGiQSvXnMBOBrnxh-VU',
-        dest_path='model/model_final.sav',
-        showsize=True
+        file_id=file_id,
+        dest_path=destination,
+        overwrite=True
     )

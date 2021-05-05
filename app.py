30cbda53b3d96e98006cef5db3c8a995e92334f9
trianggadios <trianggadio@gmail.com>
2021-05-06 00:21:59 +0700

    handle error

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import render_template
 from flask import request
+from flask import session
 
 from module import model_config
 
@@ -23,6 +24,7 @@ auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+session['loaded'] = 'false'
 
 @app.route('/')
 def index():
@@ -32,11 +34,15 @@ def index():
 @app.route('/download_model', methods=['GET'])
 def model_doc2vec_download():
     try:
-        model_config.download_all_model()
-        global doc2vec_model
-        doc2vec_model = Doc2Vec.load('model/model.d2v')
-        global clf_randomforest_model
-        clf_randomforest_model = pickle.load(open('model/model_final.sav', 'rb'))
+        if session['loaded'] == 'true':
+            pass
+        else:
+            model_config.download_all_model()
+            global doc2vec_model
+            doc2vec_model = Doc2Vec.load('model/model.d2v')
+            global clf_randomforest_model
+            clf_randomforest_model = pickle.load(open('model/model_final.sav', 'rb'))
+            session['loaded'] = 'true'
         return jsonify(
             {
                 'status': 200,
